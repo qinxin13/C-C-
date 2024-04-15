deba402e546eb726925534fc6873f24ad5149bd6
qinxin13 <3121438315@qq.com>
2024-04-15 19:54:58 +0800

    新增prntval.c和longstrg.c文件

---
+++ 学习-练习/prntval.c
@@ -0,0 +1,16 @@
+//#include<stdio.h>
+//
+////测定printf（）函数的返回值
+//int main() {
+//
+//	int val = 212;
+//
+//	int rv;
+//
+//	rv = printf("%d F是谁的沸点\n", val);
+//
+//	printf("printf()函数的的输出值是：%d\n", rv);//输出的值为16
+//
+//	return 0;
+//
+//}
