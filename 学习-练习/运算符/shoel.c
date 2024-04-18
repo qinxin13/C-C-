f57b5a5992a26661885a89afcee2a2c9dbf65db9
qinxin13 <3121438315@qq.com>
2024-04-18 15:12:39 +0800

    新增运算符练习

---
+++ 学习-练习/运算符/shoel.c
@@ -0,0 +1,22 @@
+////该代码旨在将鞋码转换成英寸
+//#include<stdio.h>
+//
+//#define ADJUST 7.31  //字符常量
+//
+//int main() {
+//
+//	const double SCALE = 0.333;//const变量
+//
+//	double shoe, foot;
+//
+//	shoe = 9.0;
+//
+//	foot = SCALE * shoe + ADJUST;
+//
+//	printf("shoe size (men's) foot length\n");
+//
+//	printf("%10.1f %15.2f inches\n", shoe, foot);
+//
+//	return 0;
+//
+//}
---
+++ 学习-练习/运算符/squares.c
@@ -0,0 +1,16 @@
+//#include<stdio.h>
+//
+////打印数字1~20极其平方
+//int main() {
+//
+//	int num = 1;
+//
+//	while (num < 21) {
+//
+//		printf("%4d%6d\n", num, num * num);
+//		num = num + 1;
+//	}
+//
+//	return 0;
+//
+//}
